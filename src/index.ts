b8d475e42d5a50227d03ec664216256ee7c67899
teleskop150750 <teleskop150750@gmail.com>
2025-11-12 23:23:40 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+const server = Bun.serve({
+  port: 5000,
+  websocket: {
+    data: {} as {
+      id: number;
+      name: string;
+    },
+    open(ws) {
+      ws.subscribe(`broadcast:${ws.data.id}`);
+    },
+    close(ws, code, reason) {
+      console.log(`WebSocket connection closed: ${code} - ${reason}`);
+    },
+    message(ws, message) {
+      ws.publish(`broadcast:${ws.data.id}`, `Echo: ${message}`);
+    },
+  },
+  routes: {
+    "/": () => new Response(Bun.file("./assets/index.html")),
+    "/ws-client": () => new Response(Bun.file("./assets/client.html")),
+    "/ws-sender": () => new Response(Bun.file("./assets/sender.html")),
+    "/broadcast/:id/:name": (req, server) => {
+      server.upgrade(req, {
+        data: { id: Number(req.params.id), name: req.params.name },
+      });
+    },
+    "/send-event/:id": (req) => {
+      const id = Number(req.params.id);
+      const message = `Hello to WebSocket ID ${id}`;
+      server.publish(`broadcast:${id}`, message);
+      return new Response(`Sent message to WebSocket ID ${id}`);
+    },
+  },
+});
+
+console.log(`Listening on ${server.url}`);
